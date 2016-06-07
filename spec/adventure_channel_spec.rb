dac63df6edada22b2567fd01b43c8e297fdc2c3c
TheNotary <no@email.plz>
2016-06-07 15:47:38 -0700

    Corrects bug where integration test wasn't being skipped correctly in absence of ircd

--- spec/adventure_channel_spec.rb
+++ spec/adventure_channel_spec.rb
@@ -5,10 +5,6 @@ describe AdventureChannel do
     expect(AdventureChannel::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    # binding.pry
-  end
-
   # 1)  Launch the IRC bot into the irc server specified in application.yml
   # 2)  Launch a test client/ admin bot into that channle too so it can do messages
   # 3)  Go through the full set of 'features' all in this one massive test
@@ -18,72 +14,69 @@ describe AdventureChannel do
   # part of the code base will likely be a mess of merge conflicts so try to
   # bear that in mind as you wait for PRs to be merged.
   it 'can have a monster attack the channel' do
-    return if !$IsIRCServerListening # skip this test if they can't connect to IRC
+    if $IsIRCServerListening # skip this test if they can't connect to IRC
 
-    @irc_game = launch_adventure_channel_bot
+      @irc_game = launch_adventure_channel_bot
 
-    @irc_admin = launch_admin_bot
-    channel = @irc_admin.channels.first
-
-
-    adventure_channel_user = get_user_from_name(@irc_admin, ENV['irc_nick'])
-
-    # Can pull up inventory
-    resetMessageBuffers
-    adventure_channel_user.send "!inventory"
-    wait_for_messages_pm(3)
-
-    # TODO:  Figure out why this test sometiems fails...
-    binding.pry if $MessagesPM.first != "Backpack Contents:"
-
-    expect($MessagesPM.first).to eq("Backpack Contents:")
+      @irc_admin = launch_admin_bot
+      channel = @irc_admin.channels.first
 
 
-    # Can Initiate a battle
-    resetMessageBuffers
-    adventure_channel_user.send "!initiate_battle #{ENV['auth_token']}"
-    # channel.send "!initiate_battle #{ENV['auth_token']}"
-    wait_for_messages_pm(1)
+      adventure_channel_user = get_user_from_name(@irc_admin, ENV['irc_nick'])
 
-    expect($MessagesPM.last).to eq("A battle has started")
+      # Can pull up inventory
+      resetMessageBuffers
+      adventure_channel_user.send "!inventory"
+      wait_for_messages_pm(3)
 
-    # TODO: test fails sometimes...
-    # A mob can beginning stealing from the channel's treasury
-    binding.pry
+      # TODO:  Make it so tests relating to $MessagesPM don't sometimes fail
+      expect($MessagesPM.first).to eq("Backpack Contents:")
 
-    # TODO: test fails sometimes...
-    # A mob can attack a player
 
-    # Can Attack a mob and win a battle
-    mob = $Game.battle.mobs.first
-    mob.hp = 1
-    mob.save
-    resetMessageBuffers
-    channel.send "!fight"
+      # Can Initiate a battle
+      resetMessageBuffers
+      adventure_channel_user.send "!initiate_battle #{ENV['auth_token']}"
+      # channel.send "!initiate_battle #{ENV['auth_token']}"
+      wait_for_messages_pm(1)
 
-    wait_for_messages_channel(3)
+      expect($MessagesPM.last).to eq("A battle has started")
+
+      # TODO: Write mob stealing from the channel's treasury
+      # A mob can beginning stealing from the channel's treasury
+      binding.pry
+
+      # TODO: Write test for A mob can attack a player
+
+      # Can Attack a mob and win a battle
+      mob = $Game.battle.mobs.first
+      mob.hp = 1
+      mob.save
+      resetMessageBuffers
+      channel.send "!fight"
+
+      wait_for_messages_channel(3)
 
 
 
-    expect($MessagesChannel[0]).to eq("[#{@irc_admin.nick}] dmgs the Green Goblin for 1pt")
-    expect($MessagesChannel[1]).to eq(">> The Green Goblin is slain <<")
-    expect($MessagesChannel[2]).to eq(">>~ The Battle is Won ~<<")
+      expect($MessagesChannel[0]).to eq("[#{@irc_admin.nick}] dmgs the Green Goblin for 1pt")
+      expect($MessagesChannel[1]).to eq(">> The Green Goblin is slain <<")
+      expect($MessagesChannel[2]).to eq(">>~ The Battle is Won ~<<")
 
-    # You can recieve loot from slain mobs
-
-
-    # Can gain exp from killing a mob and level up
-    wait_for_messages_pm(1)
-    expect($MessagesPM.last).to eq("you earned 2xp")
-
-    # TODO: implement leveling up
-    # You can check your status and see your level
+      # TODO: You can recieve loot from slain mobs
 
 
-    # You can equip items and change the amount of damage you do
+      # Can gain exp from killing a mob and level up
+      wait_for_messages_pm(1)
+      expect($MessagesPM.last).to eq("you earned 2xp")
 
-    #
+      # TODO: implement leveling up
+      # You can check your status and see your level
 
+      # TODO:
+      # You can equip items and change the amount of damage you do
+
+      #
+    end
 
   end
 end
