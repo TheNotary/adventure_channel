97953fa8dc47cad4a51f66eff1617a04f3fda2fc
TheNotary <no@email.plz>
2016-06-10 13:52:01 -0700

    introduces singleton nameing convention _singleton.rb to drift important files to top of folder

--- lib/adventure_channel/adventure_game.rb
+++ lib/adventure_channel/adventure_game.rb
@@ -1,8 +1,8 @@
-require 'adventure_channel/adventure_game/game'
+require 'adventure_channel/adventure_game/_game'
 
 require 'adventure_channel/adventure_game/user'
 
-require 'adventure_channel/adventure_game/battle/battle'
+require 'adventure_channel/adventure_game/battle/_battle'
 require 'adventure_channel/adventure_game/battle/battleable'
 require 'adventure_channel/adventure_game/battle/mob'
 require 'adventure_channel/adventure_game/battle/battle_mathematics'
